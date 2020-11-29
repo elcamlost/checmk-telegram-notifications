3be0062aea8c61ba03e7adc0c64ebde99abda67b
Thierry Trafelet <thierry.tra@bluewin.ch>
2020-11-29 14:12:55 +0100

    Put notifier into classes

--- notifications/telegram.py
+++ notifications/telegram.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Telegram
 
-# TODO: Put this in classes. Currently it is pretty messy
+# TODO: Test whether the new code works
 import os
 import json
 import requests
@@ -10,13 +10,16 @@ from sys import stderr, exit as s_exit
 from cmk.notification_plugins import utils
 from cmk.notification_plugins.mail import render_performance_graphs
 
-CHAT_ID_FIELD_NAMES = [
-    "PARAMETER_TELEGRAM_CHAT_ID",  # for notification parameters
-    "CONTACT_TELEGRAM_CHAT_ID"  # for custom attributes
-]
-BOT_TOKEN_FIELD = "PARAMETER_TELEGRAM_BOT_TOKEN"
-GRAPH_CONFIG_FIELD = "PARAMETER_TELEGRAM_GRAPH_CONFIG"
-HOST_TEMPLATE = """<b>$NOTIFICATIONTYPE$: <a href="%s">$HOSTNAME$</a> $EVENT_TXT$</b>
+
+class TelegramConfig():
+    graph_config_field_name = "PARAMETER_TELEGRAM_GRAPH_CONFIG"
+    bot_token_field_name = "PARAMETER_TELEGRAM_BOT_TOKEN"
+    chat_id_field_names = [
+        "PARAMETER_TELEGRAM_CHAT_ID",  # for notification parameters
+        "CONTACT_TELEGRAM_CHAT_ID"  # for custom attributes
+    ]
+
+    notification_host_template = """<b>$NOTIFICATIONTYPE$: <a href="%s">$HOSTNAME$</a> $EVENT_TXT$</b>
 <code>
 Host:     $HOSTNAME$
 Alias:    $HOSTALIAS$
@@ -28,7 +31,7 @@ Detail:
 $LONGHOSTOUTPUT$
 </code>"""
 
-SERVICE_TEMPLATE = """<b>$NOTIFICATIONTYPE$: <a href="%s">$HOSTNAME$/$SERVICEDESC$</a> $EVENT_TXT$</b>
+    notification_service_template = """<b>$NOTIFICATIONTYPE$: <a href="%s">$HOSTNAME$/$SERVICEDESC$</a> $EVENT_TXT$</b>
 <code>
 Host:     $HOSTNAME$
 Alias:    $HOSTALIAS$
@@ -41,156 +44,176 @@ Detail:
 $LONGSERVICEOUTPUT$
 </code>"""
 
+    def __init__(self):
+        self.__context = utils.collect_context()
+        self.__bot_token = None
+        self.__chat_id = None
 
-def telegram_bot_token(context):
-    if BOT_TOKEN_FIELD in context:
-        return context[BOT_TOKEN_FIELD]
-    raise AttributeError("Unable to find context variable '%s'" %
-                         BOT_TOKEN_FIELD)
+    def _replace_newlines(self, text):
+        return text.replace("\\n", "\n")
 
+    @property
+    def __is_service_notification(self):
+        return self.__context["WHAT"] == "SERVICE"
 
-def telegram_chat_id(context):
-    for fieldname in CHAT_ID_FIELD_NAMES:
-        if fieldname in context and context[fieldname] != 0:
-            return context[fieldname]
-    raise AttributeError("Unable to find chat ID in any field: %s" %
-                         ",".join(CHAT_ID_FIELD_NAMES))
+    @property
+    def __notification_status(self):
+        if not self.__notification_status:
+            if self.__is_service_notification:
+                return int(self.__context["SERVICESTATEID"])
+            else:
+                return int(self.__context["HOSTSTATEID"])
 
+    @property
+    def should_send_graph(self):
+        send_list = []
 
-def telegram_url_for(command, context, hide_token=False):
-    return "https://api.telegram.org/bot%s/%s" % (
-        telegram_bot_token(context) if not hide_token else "****", command)
+        for setting in filter(lambda e: e.startswith(self.graph_config_field_name), self.__context):
+            if self.__context[setting] == "True":
+                # variables are named <GRAPH_CONFIG_FIELD>_X where X is the checkmk status ID
+                send_list.append(int(setting[-1]))
 
+        return self.__notification_status in send_list
 
-def telegram_command(endpoint, context, files=None, **kwargs):
-    json_data = dict({"chat_id": telegram_chat_id(context)}, **kwargs)
+    @property
+    def performance_graphs(self):
+        return render_performance_graphs(self.__context)
 
-    if not files:
-        response = requests.post(url=telegram_url_for(endpoint, context),
-                                 json=json_data,
-                                 files=files)
-    else:  # when sending files we need to also send parameters using multipart/form-data
-        # stderr.write(repr(requests.Request("POST", telegram_url_for(endpoint, context), files=files).prepare().body.decode("ascii")))
-        response = requests.post(url=telegram_url_for(endpoint, context),
-                                 data=json_data,
-                                 files=files)
+    @property
+    def bot_token(self):
+        if not self.__bot_token:
+            if self.bot_token_field_name in self.__context:
+                self.__bot_token = self.__context[self.bot_token_field_name]
+            raise AttributeError("Unable to find context variable '%s'" %
+                                 self.bot_token_field_name)
+        return self.__bot_token
 
-    if response.status_code != 200:
-        raise Exception(
-            "%i: %s -> Unable to call %s. JSON Data: %s, Files: %s" % (
-                int(response.status_code),
-                response.reason,
-                telegram_url_for(
-                    endpoint, context,
-                    hide_token=True),  # do not log the token value
-                repr(json_data),
-                str(files)[:50]))
+    @property
+    def chat_id(self):
+        if self.__chat_id:
+            for fieldname in self.chat_id_field_names:
+                if fieldname in self.__context and self.__context[
+                        fieldname] != 0:
+                    self.__chat_id = self.__context[fieldname]
+            raise AttributeError("Unable to find chat ID in any field: %s" %
+                                 ",".join(self.chat_id_field_names))
+        return self.__chat_id
 
-
-def telegram_send_message(context, text):
-    telegram_command(
-        "sendMessage", context, **{
-            "text": text,
-            "disable_web_page_preview": True,
-            "parse_mode": "html"
-        })
-
-
-def telegram_send_photo(context, caption, photo_data):
-    telegram_command("sendPhoto",
-                     context,
-                     files={"photo": photo_data},
-                     **{
-                         "parse_mode": "html",
-                         "caption": caption
-                     })
-
-
-def telegram_send_mediagroup(context, photo_data, media_description):
-    """
-    Send multiple media to telegram as one album.
-    photo_data => a key-value mapping in the format 'filename': 'image data'
-    media_description => a list of dicts telling telegram, what images are in photo_data
-
-    https://core.telegram.org/bots/api#sendmediagroup
-    https://core.telegram.org/bots/api#inputmediaphoto
-    https://github.com/php-telegram-bot/core/issues/811
-    """
-
-    telegram_command(
-        "sendMediaGroup",
-        context,
-        files=photo_data,
-        **{
-            "disable_notification": True,
-            "media": json.dumps(media_description)
-            # "media": [ { "type": "photo", "media": "attach://photo_1" }, ... ]
-            # --- from files parameter:
-            # "photo_1": binary data
-            # "photo_2": binary data
-        })
-
-
-def should_send_graph(context, notification_status):
-    send_list = []
-
-    for setting in filter(lambda e: e.startswith(GRAPH_CONFIG_FIELD), context):
-        if context[setting] == "True":
-            # variables are named <GRAPH_CONFIG_FIELD>_X where X is the checkmk status ID
-            send_list.append(int(setting[-1]))
-
-    return notification_status in send_list
-
-
-def replace_newlines(text):
-    return text.replace("\\n", "\n")
-
-
-def main(host_template, service_template):
-    context = utils.collect_context()
-
-    if context["WHAT"] == "SERVICE":
-        text = service_template % utils.service_url_from_context(context)
-        notification_status = int(context["SERVICESTATEID"])
-    else:
-        text = host_template % utils.host_url_from_context(context)
-        notification_status = int(context["HOSTSTATEID"])
-    text = utils.substitute_context(text, context)
-    text = replace_newlines(text)
-
-    try:
-        if should_send_graph(context, notification_status):
-            # fetch images
-            attachments, _ = render_performance_graphs(context)
+    @property
+    def notification_content(self):
+        if self.__is_service_notification:
+            text = self.notification_service_template % utils.service_url_from_context(self.__context)
         else:
-            attachments = []
+            text = self.notification_host_template % utils.host_url_from_context(self.__context)
+        text = utils.substitute_context(text, self.__context)
+        text = self._replace_newlines(text)
 
-        if len(attachments
-               ) == 1:  # exactly one picture, send as photo with caption
-            _, _, att_data, _ = attachments[0]
-            telegram_send_photo(context, text, att_data)
+        return text
+    
 
-        elif len(attachments) > 1:  # more than one picture, send as album
-            telegram_media = []  # media description list for telegram API
-            media_data = {}  # the actual image data
-            for _, att_name, att_data, _ in attachments:
-                telegram_media.append({
-                    "type": "photo",
-                    "media": "attach://%s" % att_name
-                })
+class TelegramNotifier():
+    def __init__(self, config):
+        self.__config = config
 
-                media_data[att_name] = att_data
+    def _base_url(self, endpoint, hide_token=False):
+        return "https://api.telegram.org/bot%s/%s" % (
+            self.__config.bot_token if not hide_token else "****", endpoint)
 
-            # Add notification text as description
-            telegram_media[-1].update({"caption": text, "parse_mode": "html"})
+    def _api_command(self, endpoint, context, files=None, **kwargs):
+        json_data = dict({"chat_id": self.__config.chat_id}, **kwargs)
 
-            telegram_send_mediagroup(context, media_data, telegram_media)
-        else:  # no pictures, send text only
-            telegram_send_message(context, text)
+        if not files:
+            response = requests.post(url=self._base_url(endpoint),
+                                     json=json_data,
+                                     files=files)
+        else:  # when sending files we need to also send parameters using multipart/form-data
+            response = requests.post(url=self._base_url(endpoint),
+                                     data=json_data,
+                                     files=files)
 
-    except AttributeError as atterr:
-        stderr.write(repr(atterr))
-        s_exit(2)
+        if response.status_code != 200:
+            raise Exception(
+                "%i: %s -> Unable to call %s. JSON Data: %s, Files: %s" % (
+                    int(response.status_code),
+                    response.reason,
+                    self._base_url(
+                        endpoint,
+                        hide_token=True),  # do not log the token value
+                    repr(json_data),
+                    str(files)[:50]))
 
+    def _send_message(self, text):
+        self._api_command(
+            "sendMessage", **{
+                "text": text,
+                "disable_web_page_preview": True,
+                "parse_mode": "html"
+            })
 
-main(HOST_TEMPLATE, SERVICE_TEMPLATE)
+    def _send_photo(self, caption, photo_data):
+        self._api_command("sendPhoto",
+                           files={"photo": photo_data},
+                           **{
+                               "parse_mode": "html",
+                               "caption": caption
+                           })
+
+    def _send_mediagroup(self, photo_data, media_description):
+        """
+        Send multiple media to telegram as one album.
+        photo_data => a key-value mapping in the format 'filename': 'image data'
+        media_description => a list of dicts telling telegram, what images are in photo_data
+
+        https://core.telegram.org/bots/api#sendmediagroup
+        https://core.telegram.org/bots/api#inputmediaphoto
+        https://github.com/php-telegram-bot/core/issues/811
+        """
+
+        self._api_command("sendMediaGroup",
+                           files=photo_data,
+                           **{
+                               "disable_notification": True,
+                               "media": json.dumps(media_description)
+                           })
+
+    # TODO: refactor
+    def notify(self):
+        text = self.__config.notification_content()
+
+        try:
+            # TODO: this could all be put directly into "performance_graphs" function of config
+            # which would return an empty list when graphs should not be sent
+            if self.__config.should_send_graph():
+                # fetch images
+                attachments, _ = self.__config.performance_graphs
+            else:
+                attachments = []
+
+            if len(attachments
+                ) == 1:  # exactly one picture, send as photo with caption
+                _, _, att_data, _ = attachments[0]
+                self._send_photo(text, att_data)
+
+            elif len(attachments) > 1:  # more than one picture, send as album
+                telegram_media = []  # media description list for telegram API
+                media_data = {}  # the actual image data
+                for _, att_name, att_data, _ in attachments:
+                    telegram_media.append({
+                        "type": "photo",
+                        "media": "attach://%s" % att_name
+                    })
+
+                    media_data[att_name] = att_data
+
+                # Add notification text as description
+                telegram_media[-1].update({"caption": text, "parse_mode": "html"})
+
+                self._send_mediagroup(media_data, telegram_media)
+            else:  # no pictures, send text only
+                self._send_message(text)
+
+        except AttributeError as atterr:
+            stderr.write(repr(atterr))
+            s_exit(2)
+
+TelegramNotifier(TelegramConfig()).notify()
